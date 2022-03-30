fcaab5e5e86b9e4739035d7f23e5664db2947ffe
Alex Mtui <67943122+xizzmtui@users.noreply.github.com>
2022-03-30 18:28:36 +0300

    Created views for Api

--- jfasta/quizapp/views.py
+++ jfasta/quizapp/views.py
@@ -1,3 +1,175 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from http import server
+from itertools import combinations
+from pyexpat.errors import messages
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404
+from numpy import gradient
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+from .models import User, Question, Question_Options, Quiz, Quiz_Question, Feedback, ContentSuggested, Post,  Reply
+from .serializers import UserSerializer, QuizSerializer, Quiz_QuestionSerializer, QuestionSerializer, Question_OptionsSerializer,FeedbackSerializer, ContentSuggestedSerializer, PostSerializer, ReplySerializer
+from quiz_app import serializers
+
+
+# Create your views here.
+
+
+
+# API views
+
+class UserList(APIView):
+    def get(self, request):
+        User1 = User.objects.all()
+        serializer = UserSerializer(User1, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        data = JSONParser().parse(request)
+        serializer = UserSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+
+
+class QuestionList(APIView):
+    def get(self, request):
+        Question1 = Question.objects.all()
+        serializer = QuestionSerializer(Question1, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        data = JSONParser().parse(request)
+        serializer = QuestionSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+        
+
+
+class Question_OptionsList(APIView):
+    def get(self, request):
+        Question_Option = Question_Options.objects.all()
+        serializer = Question_OptionsSerializer(Question_Option, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        data = JSONParser().parse(request)
+        serializer = Question_OptionsSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+class QuizList(APIView):
+    def get(self, request):
+        Quiz1 = Quiz.objects.all()
+        serializer =QuizSerializer(Quiz1, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        data = JSONParser().parse(request)
+        serializer = QuizSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+
+class Quiz_QuestionList(APIView):
+    def get(self, request):
+        QuizQ = Quiz_Question.objects.all()
+        serializer = Quiz_QuestionSerializer(QuizQ, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        data = JSONParser().parse(request)
+        serializer = Quiz_QuestionSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+class FeedbackList(APIView):
+    def get(self, request):
+        Fdb = Feedback.objects.all()
+        serializer = FeedbackSerializer(Fdb, many=True)
+        return Response(serializer.data)
+
+
+    def post(self, request):
+        data = JSONParser().parse(request)
+        serializer = FeedbackSerializer(data=data)
+
+        if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+class ContentSuggestedList(APIView):
+    def get(self, request):
+        CSugg = ContentSuggested.objects.all()
+        serializer = ContentSuggestedSerializer(CSugg, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        data = JSONParser().parse(request)
+        serializer= ContentSuggestedSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+    
+
+class PostList(APIView):
+    def get(self,  request):
+        NewPost = Post.objects.all()
+        serializer = PostSerializer(NewPost, many=True)
+        return Response(serializer.data)
+        
+
+    def post(self, request):
+        data = JSONParser().parse(request)
+        serializer = PostSerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+class ReplyList(APIView):
+    def get(self, request):
+        NewReply = Reply.objects.all()
+        serializer = ReplySerializer(NewReply, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        data = JSONParser().parse(request)
+        serializer = ReplySerializer(data=data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+# Other Views
