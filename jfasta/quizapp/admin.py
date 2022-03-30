ed516104cc531afb18d249088d58409d0e2b1908
Alex Mtui <67943122+xizzmtui@users.noreply.github.com>
2022-03-30 18:18:17 +0300

    Register Models to the Admin Page

--- jfasta/quizapp/admin.py
+++ jfasta/quizapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from quiz_app.models import ContentSuggested, Feedback, Post, Question, Question_Options, Quiz, Quiz_Question, Reply, User
 
-# Register your models here.
+# # Register your models here.
+admin.site.register(User)
+admin.site.register(Question)
+admin.site.register(Question_Options)
+admin.site.register(Post)
+admin.site.register(Reply)
+admin.site.register(Quiz)
+admin.site.register(Quiz_Question)
+admin.site.register(ContentSuggested)
+admin.site.register(Feedback)
