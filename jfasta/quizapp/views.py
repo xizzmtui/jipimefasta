af686f4397cea73c8095ea56aab3038fc76567e1
Alex Mtui <67943122+xizzmtui@users.noreply.github.com>
2022-04-03 00:47:16 +0300

    modified login and register functionalities

--- jfasta/quizapp/views.py
+++ jfasta/quizapp/views.py
@@ -1,12 +1,14 @@
+
 from http.client import HTTPResponse
+from django import db
 from django.shortcuts import render
 
 # Create your views here.
 
 from http import server
 from itertools import combinations
-from pyexpat.errors import messages
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render,redirect
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404
 from numpy import gradient
@@ -17,6 +19,13 @@ from rest_framework import status
 from .models import User, Question, Question_Options, Quiz, Quiz_Question, Feedback, ContentSuggested, Post,  Reply
 from .serializers import UserSerializer, QuizSerializer, Quiz_QuestionSerializer, QuestionSerializer, Question_OptionsSerializer,FeedbackSerializer, ContentSuggestedSerializer, PostSerializer, ReplySerializer
 from quizapp import serializers
+import sqlite3
+from .forms import NewUserForm
+from django.contrib.auth import login, authenticate 
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.forms import AuthenticationForm
+
+
 
 
 # Create your views here.
@@ -29,11 +38,42 @@ def forum(request):
 def dashboard(request):
     return render(request, 'dashboard.html')
 
+def register(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request)
+			messages.success(request, "Registration successful." )
+			return redirect("index")
+		messages.error(request, "Unsuccessful registration. Invalid information.")
+	form = NewUserForm()
+	return render (request=request, template_name="register.html", context={"register_form":form})
+
 def login(request):
+	if request.method == "POST":
+		form = AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			username = form.cleaned_data.get('username')
+			password = form.cleaned_data.get('password')
+			user = authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}.")
+				return redirect("index")
+			else:
+				messages.error(request,"Invalid username or password.")
+		else:
+			messages.error(request,"Invalid username or password.")
+	form = AuthenticationForm()
+	return render(request=request, template_name="login.html", context={"login_form":form})
+    
+
+        
+def logout(request, user):
+    user.session['logged_in'] = False
     return render(request, 'login.html')
 
-def register(request):
-    return render(request, 'register.html')
 
 def makepost(request):
     return render(request, 'makepost.html')
