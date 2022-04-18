2303e5c922a551338ab9af6b70c7ea2894c03fd5
Alex Mtui <67943122+xizzmtui@users.noreply.github.com>
2022-04-18 15:27:02 +0300

    updated user registration

--- jfasta/quizapp/forms.py
+++ jfasta/quizapp/forms.py
@@ -9,8 +9,6 @@ from django.contrib.auth.models import User
 
 class NewUserForm(UserCreationForm):
         email = forms.EmailField(required=True)
-        firstname = forms.CharField(max_length=30, required=True)
-        lastname = forms.CharField(max_length=30, required=True)
         school = forms.CharField(max_length=40, required=True)
         level = forms.IntegerField(required=True, max_value= 6, min_value=1)
         combination = forms.CharField(max_length=3, required=True)
@@ -20,13 +18,13 @@ class NewUserForm(UserCreationForm):
 
         class Meta:
             model = User
-            fields = ("username", "email","firstname","lastname","school","combination","level", "password1", "password2",)
+            fields = ("username", "email","first_name","last_name","school","combination","level", "password1", "password2",)
 
         def save(self, commit=True):
             user = super(NewUserForm, self).save(commit=False)
             user.email = self.cleaned_data['email']
-            user.firstname = self.cleaned_data['firstname']
-            user.lastname = self.cleaned_data['lastname']
+            user.first_name = self.cleaned_data['first_name']
+            user.last_name = self.cleaned_data['last_name']
             user.school = self.cleaned_data['school']
             user.level = self.cleaned_data['level']
             user.combination = self.cleaned_data['combination']
@@ -45,11 +43,11 @@ class UserUpdateForm(forms.ModelForm):
     level = forms.IntegerField()
     combination = forms.CharField()
     username = User.username
-    password = User.password
+    
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password',  'firstname', 'lastname', 'school', 'level', 'combination']
+        fields = ['username', 'email',  'firstname', 'lastname', 'school', 'level', 'combination']
         
 
 
